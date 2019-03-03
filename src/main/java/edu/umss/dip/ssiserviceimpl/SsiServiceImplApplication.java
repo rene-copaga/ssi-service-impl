255257af1c31d423238e3dc34c47c4c6c13d406c
Rene Copaga <rene.copaga@gmail.com>
2019-03-03 16:50:04 -0400

    Spring project

---
+++ src/main/java/edu/umss/dip/ssiserviceimpl/SsiServiceImplApplication.java
@@ -0,0 +1,13 @@
+package edu.umss.dip.ssiserviceimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiServiceImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiServiceImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/umss/dip/ssiserviceimpl/SsiServiceImplApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.umss.dip.ssiserviceimpl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsiServiceImplApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
